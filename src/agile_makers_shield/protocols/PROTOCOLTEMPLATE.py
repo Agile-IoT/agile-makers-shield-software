1c97ae5247e055479cd071f6f74fbd9c584d65fc
David Palomares <d.palomares@libelium.com>
2018-08-09 10:38:17 +0200

    Changed license to EPL-2.0

--- src/agile_makers_shield/protocols/PROTOCOLTEMPLATE.py
+++ src/agile_makers_shield/protocols/PROTOCOLTEMPLATE.py
@@ -1,15 +1,12 @@
 
 ############################################################################
-# Copyright (c) 2016, 2017 Libelium Comunicaciones Distribuidas S.L.       #
+# Copyright (c) 2016-2018 Libelium Comunicaciones Distribuidas S.L.        #
 #                                                                          #
-# All rights reserved. This program and the accompanying materials         #
-# are made available under the terms of the Eclipse Public License v1.0    #
-# and Eclipse Distribution License v1.0 which accompany this distribution. #
+# This program and the accompanying materials are made                     #
+# available under the terms of the Eclipse Public License 2.0              #
+# which is available at https://www.eclipse.org/legal/epl-2.0/             #
 #                                                                          #
-# The Eclipse Public License is available at                               #
-#    http://www.eclipse.org/legal/epl-v10.html                             #
-# and the Eclipse Distribution License is available at                     #
-#   http://www.eclipse.org/org/documents/edl-v10.php.                      #
+# SPDX-License-Identifier: EPL-2.0                                         #
 #                                                                          #
 # Contributors:                                                            #
 #    David Palomares - Initial API and implementation                      #
